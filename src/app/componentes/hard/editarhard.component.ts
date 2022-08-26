0636165070e897890f05508b31af7f3697590b76
Seulgeant <leo_04_10@hotmail.com>
2022-08-26 18:17:09 -0300

    se corrigio componentes edit

--- src/app/componentes/hard/editarhard.component.ts
+++ src/app/componentes/hard/editarhard.component.ts
@@ -2,6 +2,7 @@ import { HardService } from './../../service/hard.service';
 import { Component, OnInit } from '@angular/core';
 import { Hard } from 'src/app/model/Hard.model';
 import { ActivatedRoute, Router } from '@angular/router';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-editarhard',
@@ -10,9 +11,24 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class EditarhardComponent implements OnInit {
   hard: Hard = null;
-  constructor(private sHard: HardService, private activatedRoute:ActivatedRoute, private route:Router) { }
+  roles: string[] = [];
+  role: string = "";
+
+  constructor(private tokenService:TokenService,private sHard: HardService, private activatedRoute:ActivatedRoute, private route:Router) { }
 
   ngOnInit(): void {
+         //verifica admin
+         if (this.tokenService.getToken()) {
+          this.roles = this.tokenService.getAuthorities();
+          for (let rol of this.roles) {
+            if (rol == "ROLE_ADMIN") {
+              this.role = "ROLE_ADMIN"
+              break;
+            }
+          }
+          if (this.role != "ROLE_ADMIN") { this.route.navigate(['']) }
+        }
+        // trae datos
     const id = this.activatedRoute.snapshot.params['id'];
     this.sHard.detail(id).subscribe(
       data =>{
--- src/app/componentes/proyectos/modaledit/modaledit.component.ts
+++ src/app/componentes/proyectos/modaledit/modaledit.component.ts
@@ -2,6 +2,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Proyecto } from 'src/app/model/proyecto.model';
 import { ProyectoService } from 'src/app/service/proyecto.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-modaledit',
@@ -10,9 +11,24 @@ import { ProyectoService } from 'src/app/service/proyecto.service';
 })
 export class ModaleditComponent implements OnInit {
   proyupd: Proyecto = null;
-  constructor(private sProyecto: ProyectoService, private activatedRoute:ActivatedRoute, private route:Router) { }
+  roles: string[] = [];
+  role: string = "";
+
+  constructor(private tokenService:TokenService,private sProyecto: ProyectoService, private activatedRoute:ActivatedRoute, private route:Router) { }
 
   ngOnInit(): void {
+            //verifica admin
+            if (this.tokenService.getToken()) {
+              this.roles = this.tokenService.getAuthorities();
+              for (let rol of this.roles) {
+                if (rol == "ROLE_ADMIN") {
+                  this.role = "ROLE_ADMIN"
+                  break;
+                }
+              }
+              if (this.role != "ROLE_ADMIN") { this.route.navigate(['']) }
+            }
+            // trae datos
     const id = this.activatedRoute.snapshot.params['id'];
     this.sProyecto.detail(id).subscribe(
       data =>{
